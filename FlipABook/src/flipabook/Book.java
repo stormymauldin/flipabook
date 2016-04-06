0ee05e3dcbb585cf8019077705125401806ccd7b
William Mauldin <stormymauldin@utexas.edu>
2016-04-06 08:16:19 -0500

    cleaned up workspace

--- FlipABook/src/flipabook/Book.java
+++ FlipABook/src/flipabook/Book.java
@@ -1,52 +1,58 @@
 package flipabook;
 
 import java.util.ArrayList;
-
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
 @Entity
-public class Book implements Comparable<Book>{
-	
+public class Book implements Comparable<Book> {
 	@Id
 	Long id;
-	private String title;
-	private String isbn;
-	private ArrayList<String> tags;
+	String title;
+	String isbn;
+	ArrayList<String> tags;
 	
-	public Book(String title, String isbn){
+	public Book(){}
+
+	public Book(String title, String isbn) {
 		this.title = title;
 		this.isbn = isbn;
 		tags = new ArrayList<String>();
 	}
-	
-	public Book(String title, String isbn, ArrayList<String> tags){
+
+	public Book(String title, String isbn, String tag) {
+		this(title, isbn);
+		tags.add(tag);
+	}
+
+	public Book(String title, String isbn, ArrayList<String> tags) {
 		this(title, isbn);
 		this.tags = tags;
 	}
-	
-	public void addTag(String tag){
-		tags.add(tag);
-	}
-	
-	public String getTitle(){
+
+	public String getTitle() {
 		return title;
 	}
-	
-	public String getIsbn(){
+
+	public String getIsbn() {
 		return isbn;
 	}
-	
-	public ArrayList<String> getTag(){
+
+	public ArrayList<String> getTags() {
 		return tags;
+	}
+
+	public void addTag(String tag) {
+		tags.add(tag);
 	}
 
 	@Override
 	public int compareTo(Book other) {
 		if (isbn.equals(other.getIsbn())) {
+			// books are the same
 			return 0;
-		} else {
-			return 1;
 		}
+		// posts are not the same
+		return 1;
 	}
 }
--- FlipABook/src/flipabook/Post.java
+++ FlipABook/src/flipabook/Post.java
@@ -1,5 +1,3 @@
-//http://tyestormyblog.appspot.com
-
 package flipabook;
 
 import java.util.Date;
@@ -9,61 +7,87 @@ import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class Post implements Comparable<Post> {
-
 	@Id
 	Long id;
-	User user;
 	String title;
-	String content;
-	Date date;
+	String description;
+	FlipABookUser seller;
 	Book book;
-
-	@SuppressWarnings("unused")
-	private Post() {
-	}
-
-	public Post(User user, String title, String content) {
-		this.user = user;
-		this.title = title;
-		this.content = content;
-		date = new Date();
-	}
+	double price;
+	Date date;
+	Date deadline;
+	int status;
+	public static final int ACTIVE = 0;
+	public static final int SUSPENDED = 1;
+	private static final long DAY_IN_MS = 1000 * 60 * 60 * 24;
+	private static final long TWO_WEEKS = 14 * DAY_IN_MS;
 	
-	public Post(User user, String title, String content, Book book) {
-		this.user = user;
-		this.title = title;
-		this.content = content;
-		this.book = book;
-		date = new Date();
-	}
+	public Post(){}
 
-	public User getUser() {
-		return user;
+	public Post(FlipABookUser seller, Book book, double price, String title, String description) {
+		this.seller = seller;
+		this.book = book;
+		this.price = price;
+		this.title = title;
+		this.description = description;
+		date = new Date();
+		deadline = new Date(date.getTime() + TWO_WEEKS);
+		status = ACTIVE;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public String getContent() {
-		return content;
+	public String getDescription() {
+		return description;
+	}
+
+	public FlipABookUser getSeller() {
+		return seller;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public double getPrice() {
+		return price;
 	}
 
 	public Date getDate() {
 		return date;
 	}
-	
-	public String getEmailablePost(){
-		return title + ":\n\n" + content + "\n\nPosted by " + user.getNickname() + " on " + date + ".";
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void editTitle(String newTitle) {
+		title = newTitle;
+	}
+
+	public void editDescription(String newDescription) {
+		description = newDescription;
+	}
+
+	public void editPrice(double newPrice) {
+		price = newPrice;
+	}
+
+	public void editStatus(int newStatus) {
+		if (newStatus == ACTIVE || newStatus == SUSPENDED) {
+			status = newStatus;
+		}
 	}
 
 	@Override
 	public int compareTo(Post other) {
-		if (date.after(other.date)) {
-			return 1;
-		} else if (date.before(other.date)) {
-			return -1;
+		if (book.compareTo(other.getBook()) == 0 && seller.compareTo(other.getSeller()) == 0) {
+			// posts are the same
+			return 0;
 		}
-		return 0;
+		// posts are not the same
+		return -1;
 	}
 }
