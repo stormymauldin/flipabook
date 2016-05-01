0cde9c5c6b5359d203a954c3c920d37f7b9387bf
William Mauldin <stormymauldin@utexas.edu>
2016-05-01 16:23:12 -0500

    Integrated Daniel's changes

--- FlipABook/src/objects/HomePage.java
+++ FlipABook/src/objects/HomePage.java
@@ -10,6 +10,7 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.User;
 
@@ -114,7 +115,8 @@ public class HomePage {
 			
 			//Initializes all messages on the server
 		    Query message_query = new Query("Message").addSort("convoID", Query.SortDirection.DESCENDING);
-			List<Entity> datastore_messages = datastore.prepare(message_query).asList(FetchOptions.Builder.withLimit(1000));
+			List<Entity> datastore_messages = datastore.prepare(message_query).asList(FetchOptions.Builder.withLimit(100000));
+			ArrayList<Key> deleted_keys = new ArrayList<Key>();
 			for (Entity message: datastore_messages){
 				Date messDate = (Date) message.getProperty("date");
 				User sender = (User) message.getProperty("sender");
@@ -126,8 +128,13 @@ public class HomePage {
 					messages.add(temp_message);
 					temp_convo.messages.add(temp_message);
 				}
+				else {
+					deleted_keys.add(message.getKey());
+					System.out.println("Zombie message found: " + message.getProperty("content"));
+				}
 			}
 
+			datastore.delete(deleted_keys);
 			System.out.println("Number of Posts: " + HomePage.posts.size());
 			init = true; 
 		}
@@ -144,137 +151,6 @@ public class HomePage {
 		return null; 
 	}
 	
-	public static void basicSearch(String term) {
-
-		HashSet<String> filteredTerm = breakup(term);
-		resetSearchWeights();
-		for (int i = 0; i < posts.size(); i++) {
-			searchEachPostField(filteredTerm, filteredTerm, filteredTerm, filteredTerm, i);
-		}
-		collectMatchingPosts();
-	}
-
-	public static void advancedSearch(String title, String author, String isbn, String keywords) {
-		HashSet<String> filteredTitle = null;
-		HashSet<String> filteredAuthor = null;
-		HashSet<String> filteredIsbn = null;
-		HashSet<String> filteredKeywords = null;
-		formatSearches(title, author, isbn, keywords, filteredTitle, filteredAuthor, filteredIsbn, filteredKeywords);
-		resetSearchWeights();
-		for (int i = 0; i < posts.size(); i++) {
-			searchEachPostField(filteredTitle, filteredAuthor, filteredIsbn, filteredKeywords, i);
-		}
-		collectMatchingPosts();
-	}
-
-	private static void collectMatchingPosts() {
-		int max = 0;
-		int maxIndex = getMaxWeightIndex(max);
-		if (maxIndex == -1) {
-			return;
-		} else {
-			while (maxIndex != -1) {
-				max = searchResultsWeighted.get(maxIndex);
-				searchResultsWeighted.set(maxIndex, 0);
-				searchResults.add(posts.get(maxIndex));
-				maxIndex = getMaxWeightIndex(max);
-			}
-		}
-	}
-
-	private static int getMaxWeightIndex(int max) {
-		int indexOfNewMax = -1;
-		for (int i = 0; i < searchResultsWeighted.size(); i++) {
-			if (searchResultsWeighted.get(i) > max) {
-				max = searchResultsWeighted.get(i);
-				indexOfNewMax = i;
-			}
-		}
-		return indexOfNewMax;
-	}
-
-	private static void formatSearches(String title, String author, String isbn, String keywords,
-			HashSet<String> filteredTitle, HashSet<String> filteredAuthor, HashSet<String> filteredIsbn,
-			HashSet<String> filteredKeywords) {
-		if (!(title == null || title == "")) {
-			filteredTitle = breakup(title);
-		}
-		if (!(author == null || author == "")) {
-			filteredAuthor = breakup(author);
-		}
-
-		if (!(isbn == null || isbn == "")) {
-			filteredIsbn = breakup(isbn);
-		}
-
-		if (!(keywords == null || keywords == "")) {
-			filteredKeywords = breakupNoArticleRemoval(keywords);
-		}
-	}
-
-	private static void searchEachPostField(HashSet<String> title, HashSet<String> author, HashSet<String> isbn,
-			HashSet<String> keywords, int postID) {
-		if (title != null) {
-			HashSet<String> postTitle = breakup(posts.get(postID).getBook().getTitle());
-			compareSets(title, postTitle, postID);
-		}
-
-		if (author != null) {
-			HashSet<String> postAuthor = breakup(posts.get(postID).getBook().getAuthor());
-			compareSets(author, postAuthor, postID);
-		}
-
-		if (isbn != null) {
-			HashSet<String> postIsbn = breakup(posts.get(postID).getBook().getIsbn());
-			compareSets(isbn, postIsbn, postID);
-		}
-
-		if (keywords != null) {
-			HashSet<String> postDescription = breakup(posts.get(postID).getDescription());
-			compareSets(keywords, postDescription, postID);
-		}
-	}
-
-	private static void resetSearchWeights() {
-		searchResultsWeighted = new ArrayList<Integer>();
-		for (Post post : posts) {
-			searchResultsWeighted.add(0);
-		}
-		searchResults = new ArrayList<Post>();
-	}
-
-	private static void compareSets(HashSet<String> search, HashSet<String> original, int postID) {
-		for (String word : search) {
-			// if the word is contained within the specified area of search, add
-			// weight to this post
-			if (original.contains(word)) {
-				searchResultsWeighted.set(postID, searchResultsWeighted.get(postID) + 1);
-			}
-		}
-
-	}
-
-	private static HashSet<String> breakup(String original) {
-		HashSet<String> removedArticles = breakupNoArticleRemoval(original);
-		while (removedArticles.contains("the") || removedArticles.contains("a") || removedArticles.contains("an")
-				|| removedArticles.contains("in") || removedArticles.contains("of")) {
-			removedArticles.remove("the");
-			removedArticles.remove("a");
-			removedArticles.remove("an");
-			removedArticles.remove("in");
-			removedArticles.remove("of");
-		}
-		return removedArticles;
-	}
-
-	private static HashSet<String> breakupNoArticleRemoval(String original) {
-		original.toLowerCase();
-		String originalKeyWords[] = original.split("\\P{Alpha}+");
-		HashSet<String> filtered = new HashSet<String>();
-		filtered.addAll(Arrays.asList(originalKeyWords));
-		return filtered;
-	}
-
 	public static FlipABookUser getUser(User user) {
 		boolean found = false;
 		for (FlipABookUser find_user: flipABookUsers){
@@ -284,14 +160,6 @@ public class HomePage {
 			}
 			
 		}
-		//Whoever made this search function is bad and should feel bad
-//		for (int i = 0; i < HomePage.users.size(); i++) {
-//			if (users.get(i).compareTo(user) == 0) {
-//				index = i;
-//				return flipABookUsers.get(index);
-////				break;
-//			}
-//		}
 		if (!found) {
 			createUser(user);
 		}
@@ -300,9 +168,6 @@ public class HomePage {
 	}
 
 	public static void createUser(User user) {
-		//FlipABookUser flipABookUser = null;
-		// TODO: other stuff here to add to objectify
-		// TODO: check to see if user has email address
 		flipABookUsers.add(new FlipABookUser(user));
 	}
 
@@ -321,7 +186,6 @@ public class HomePage {
 				deletePost(post);
 			}
 
-			// TODO: delete the user's objectify data
 		}
 
 	}
@@ -336,9 +200,5 @@ public class HomePage {
 				posts.remove(posts.indexOf(curPost));
 			}
 		}
-	}
-
-	public void displayPosts() {
-		// TODO: print out posts to view
 	}
 }
